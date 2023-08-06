e55a3cd4bba63851a2431d0fff08af4f39ff9a79
Ratna-lodh <112805822+Ratna-lodh@users.noreply.github.com>
2023-08-06 23:22:26 +0600

    Create file2.py

---
+++ file2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# Code to read csv file into Colaboratory:
+!pip install -U -q PyDrive
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+# Authenticate and create the PyDrive client.
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
